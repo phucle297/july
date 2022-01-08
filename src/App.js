37fedfad1cf09635642d1331f27926f164a67c7b
phucle297 <somdp2907@gmail.com>
2022-01-08 07:35:25 +0700

    done header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { createBrowserHistory } from "history";
+import {
+  Route,
+  Routes,
+  unstable_HistoryRouter as HistoryRouter,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import HomeTemplate from "./templates/HomeTemplate";
+export const history = createBrowserHistory();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //? Use the HistoryRouter and library history to navigate from outside components
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route path="/" element={<HomeTemplate />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </HistoryRouter>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,80 @@
+import React, { memo } from "react";
+import { Menu, Dropdown, Space } from "antd";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+const menu = (
+  <Menu>
+    <Menu.Item key="1">
+      <NavLink to="/apartment" rel="noopener noreferrer">
+        Căn hộ cho thuê
+      </NavLink>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <NavLink to="/yoga" rel="noopener noreferrer">
+        Yoga
+      </NavLink>
+    </Menu.Item>
+  </Menu>
+);
+
+function Header() {
+  const [mobile, setMobile] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
+  React.useEffect(() => {
+    if (window.innerWidth < 768) {
+      setMobile(true);
+      setVisible(false);
+    } else {
+      setMobile(false);
+      setVisible(true);
+    }
+    window.onresize = () => {
+      if (window.innerWidth < 768) {
+        setMobile(true);
+        setVisible(false);
+      } else {
+        setMobile(false);
+        setVisible(true);
+      }
+    };
+  }, [window.innnerWidth]);
+  return (
+    <header className="header py-5">
+      <NavLink to="/" id="logo" className={mobile ? "logo-mobile" : ""}>
+        <img src="./assets/images/july.png" alt="" />
+      </NavLink>
+      <div className="container mx-auto">
+        <nav
+          className={
+            "grid mt-10 md:mt-0 grid-cols-1 md:grid-cols-5 lg:px-24 xl:px-44 place-items-center" +
+            (visible ? " visible" : " hidden")
+          }
+        >
+          <NavLink to="/" className="nav-link col-span-1">
+            Trang chủ
+          </NavLink>
+          <a className="nav-link col-span-1">Về chúng tôi</a>
+          <a className=" col-span-1"></a>
+          <Space direction="vertical" className="nav-link col-span-1">
+            <Space wrap>
+              <Dropdown overlay={menu} placement="bottomCenter">
+                <a>Dịch vụ</a>
+              </Dropdown>
+            </Space>
+          </Space>
+          <a className="nav-link col-span-1">Liên hệ</a>
+        </nav>
+        <button className="controller" onClick={() => setVisible(!visible)}>
+          <i
+            className={"fa fa-bars bars" + (visible ? " hidden" : " visible")}
+          ></i>
+          <i
+            className={"fa fa-times times" + (visible ? " visible" : " hidden")}
+          ></i>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default memo(Header);
